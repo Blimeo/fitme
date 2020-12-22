6cbbd9d80378b97f7b5df3e857c58a33cbc821ec
Matthew Ye <my1@berkeley.edu>
2020-12-21 20:27:55 -0500

    Add basic routing

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,38 +1,40 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from 'react'
+import { AppBar, Tabs, Tab, Button, Toolbar, Typography, makeStyles, createStyles, Theme } from '@material-ui/core'
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import './css/Header.css';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
-}));
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        expand: {
+            flexGrow: 1,
+        },
+    }),
+);
 
-export default function Main() {
+function Nav() {
     const classes = useStyles();
     return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">
+                        Fitme
+                    </Typography>
+                    <Tabs className={classes.expand}>
+                    <Link to="/"><Tab label="Home" /></Link>
+                    <Link to="/about"><Tab label="About" /></Link>
+                    <Link to="/items"><Tab label="Items" /></Link>
+                    <Link to="/fits"><Tab label="Fits" /></Link>
+                    </Tabs>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
 
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" className={classes.title}>
-                    Fitme
-                </Typography>
-                <Button color="inherit">Login</Button>
-            </Toolbar>
-        </AppBar>
-    );
 }
+
+export default Nav;
